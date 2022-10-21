a8c12be0d056cd9a443e45a2e7c48d67de4e3aa1
azuchi <azuchi@chaintope.com>
2022-10-21 18:02:44 +0900

    Implement Taro::AssetCommitment

--- lib/taro/commitment/asset.rb
+++ lib/taro/commitment/asset.rb
@@ -6,19 +6,16 @@ module Taro
   # Assets within this tree, which are leaves represented as the serialized asset TLV payload,
   # are keyed by their `asset_script_key`.
   class AssetCommitment
-    attr_reader :version, :asset_id, :tree_root, :tree, :committed_assets
-
-    def initialize(version, asset_id)
-    end
+    attr_reader :version, :asset_id, :tree, :assets
 
     # @param [Array[Taro::Asset]]
     # @raise [Taro::Error]
-    def self.from_assets(assets)
+    def initialize(assets)
       max_version = 0
       asset_genesis = assets.first.genesis.id
       asset_family_key = assets.first.family_key
-      asset_map = {}
-      tree = MSSMT::Tree.new
+      @assets = assets
+      @tree = MSSMT::Tree.new
       assets.each do |asset|
         unless asset.family_key == asset_family_key
           raise Taro::Error, "asset commitment: family key mismatch"
@@ -29,11 +26,13 @@ module Taro
         unless asset.family_key.nil?
           # TODO: There should be a valid Schnorr sig over the asset ID in the family key.
         end
-        key = asset.commitment_key
         max_version = asset.version if asset.version > max_version
-        asset_map[key] = asset
+        @tree.insert(asset.commitment_key.bth, asset.leaf_node)
       end
       asset_id = asset_family_key.nil? ? asset_genesis : nil # TODO: family key based derivation
+
+      @version = max_version
+      @asset_id = asset_id
     end
   end
 end
---
+++ spec/taro/asset_commitment_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Taro::AssetCommitment do
+  let(:genesis) do
+    genesis_point = Bitcoin::OutPoint.from_txid("01" * 32, 1)
+    Taro::Genesis.new(
+      prev_out: genesis_point,
+      tag: "normal asset",
+      metadata: ["010203"].pack("H*"),
+      output_index: 1
+    )
+  end
+
+  let(:script_key) do
+    Bitcoin::Key.new(
+      pubkey:
+        "02a0afeb165f0ec36880b68e0baabd9ad9c62fd1a69aa998bc30e9a346202e078f"
+    )
+  end
+
+  let(:asset) do
+    fam_key =
+      Bitcoin::Key.new(
+        pubkey:
+          "0214ac808afb3c8f1fd0cf0804b5e6494dbbfc47d633cfc36a3857e9e6c555a183"
+      )
+    sig =
+      Schnorr::Signature.decode(
+        "7a70d30bb4e3de62878eed22ff3a633fe4b678def84a290066c408f88615d570ac534407df4e5e4ee05772e5e00a4376361dfd6caa632a3d21306f68c3e39d29".htb
+      )
+    family_key =
+      Taro::FamilyKey.new(
+        Taro::KeyDescriptor.new(public_key: Bitcoin::Key.generate),
+        fam_key,
+        sig
+      )
+    Taro::Asset.new(genesis, 741, 0, 0, script_key, family_key)
+  end
+
+  describe "#new" do
+    it do
+      asset_commitment = described_class.new([asset])
+      expect(asset_commitment.tree.root_hash.bth).to eq(
+        "406785de1d71bb5cf9d1c1865835ba19cd769ff3b0755a1f3eaa5a14881ce226"
+      )
+    end
+  end
+end
